90d251d2f401d40407c25709fe2e4530d601e07c
dhb <dhb@0a70a659-4692-4144-9d9b-20b559e88c6c>
2012-06-05 15:42:21 +0000

    Create xOld directory for depricated routines.  In the Brainard lab, startup.m removes any directory called xOld from Matlab's path.
    DHB

--- Contents.m
+++ Contents.m
@@ -1,10 +1,5 @@
 % WavefrontOpticsToolbox
 %
-% Programs (demonstrate use)
-%   s_wvfComputeAverageObserverConePSF -      Compute optimized cone psf for an average observer. 
-%   s_wvfComputeConePSFTest -                 Test the cone/spectrum weighted psf routines.
-%   s_wvfComputePSFTest -                     Basic test that underlying Zernike monochromatic psf routines behave sensibly.
-%
 % Basic operations (subdir wvf).  These are the core functions that know about the internals of the
 % wvf structure.  User code should generally use these and not rely on the specific
 % fieds within the structure.
@@ -13,10 +8,21 @@
 % Note, however, that the ComputePSF routine will force a compute of the pupil function
 % if has not already been computed.
 %   wvf/wvfCreate -                           Create wavefront optics (wvf) structure
-%   wvf/wvfGet    -                           Get value from wvf structure
-%   wvf/wvfSet    -                           Set value in wvf structure
 %   wvf/wvfComputePupilFunction -             Compute the pupil function for current structure values.
 %   wvf/wvfComputePSF -                       Compute the psf for current structure values.
+%   wvf/wvfGet    -                           Get value from wvf structure
+%   wvf/wvfSet    -                           Set value in wvf structure
+%
+% (c) Wavefront Toolbox Team 2011, 2012
+%
+% ****
+%
+% Needs updating below here.
+%
+% Programs (demonstrate use)
+%   s_wvfComputeAverageObserverConePSF -      Compute optimized cone psf for an average observer. 
+%   s_wvfComputeConePSFTest -                 Test the cone/spectrum weighted psf routines.
+%   s_wvfComputePSFTest -                     Basic test that underlying Zernike monochromatic psf routines behave sensibly.
 %
 % Wavefront (wvf) optics suite
 %   wvfComputeConePSF -                       Compute cone psfs given Zernike coeffs, pupil, accom wl, and weighting spectrum
