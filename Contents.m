7df3a1f931471d2986ad7d3b2625189e0ca32389
David Brainard <brainard@psych.upenn.edu>
2012-07-20 21:11:52 -0400

    Cleaned up Contents.m some

--- Contents.m
+++ Contents.m
@@ -1,5 +1,15 @@
 % WavefrontOpticsToolbox
 %
+% TUTORIAL SCRIPTS (subdir tutorial).  These help you learn about the code and the
+% ideas behind it.
+%  tutorial/t_wvfZernike -                    Basic wavefront optics ideas including Zernike expansion to describe wavefront aberrations
+%
+% VALIDATION SCRIPTS (subdir validate).  These test that various pieces of the toolboxes
+% do the right thing.
+%   validate/v_wvfDiffractionPSF -            Compare PSFs obtained with wavefront calcs to direct computation of diffraction limited PSFs
+%   validate/v_wvfSpatialSampling -           Compare PSFs with different spatial sampling parameters.
+%   validate/v_wvfSVNVer121TestData -         Compare PSFs computed by toolbox now to those computed long ago and far away.
+%
 % BASIC OPERATIONS (subdir wvf).  These are the core functions that know about the internals of the
 % wvf structure.  User code should generally use these and not rely on the specific
 % fieds within the structure.
@@ -15,6 +25,14 @@
 %   wvf/wvfPrint -                            Print values in wvf structure
 %   wvf/wvfSet -                              Set value in wvf structure
 %
+% UTILITY ROUTINES (subdir utility).  These perform various little useful
+% computations.  Some are passed the wvf structure, but none set its fields
+% directly or access its fields other than via wvfGet.
+%   utility/wvfComputeConePSF -               Compute the PSF seen by cones.
+%   utility/wvfDefocusDioptersToMicrons -     Convert diopters to microns for addition into j=4 Zernike coefficient.
+%   utility/wvfLCAFromWavelengthDifference -  Compute longitudinal chromatic aberration (LCA) in diopters, from wavelength difference.
+%   utility/wvfWave2idx -                     Convert wvf wavelengths to indices.
+%
 % PSF MANIPULATION AND ANALYSIS (subdir psf).  Perform operations on psfs.  Not directly tied
 % to the wvf structure.
 %   psf/psfAverageMultiple -                  Find the average of multiple psfs.
@@ -23,19 +41,9 @@
 %   psf/psfFindCriterionRadius -              Find the radius of circularly symmetric psf that contains a specfied fraction of the psf max.
 %   psf/psfFindPeak -                         Find the location of the maximum of a psf.
 %
-% UTILITY ROUTINES (subdir utility).  These perform various little useful computations, and are not
-% tied directly to the wvf structure.
-%   utility/wvfDefocusDioptersToMicrons -     Convert diopters to microns for addition into j=4 Zernike coefficient.
-%   utility/wvfLCAFromWavelengthDifference -  Compute longitudinal chromatic aberration (LCA) in diopters, from wavelength difference.
-%
-% TUTORIAL SCRIPTS (subdir tutorial).  These help you learn about the code and the
-% ideas behind it.
-%  tutorial/t_wvfZernike -                    Basic wavefront optics ideas including Zernike expansion to describe wavefront aberrations
-%
-% VALIDATION SCRIPTS (subdir validate).  These test that various pieces of the toolboxes
-% do the right thing.
-%   validate/v_wvfDiffractionPSF -            Compare PSFs obtained with wavefront calcs to direct computation of diffraction limited PSFs
-%   validate/v_wvfSpatialSampling -           Compare PSFs with different spatial sampling parameters.
+% STILES-CRAWFORD EFFECT (subdir stilescrawford).  As the name says.
+%   sceCreate -                               Create Stiles-Crawford structure with various options as to data.
+%   sceGet -                                  Get info from Stiles-Crawford structure.
 
 % (c) Wavefront Toolbox Team 2011, 2012
 %
@@ -48,19 +56,8 @@
 %   s_wvfComputePSFTest -                     Basic test that underlying Zernike monochromatic psf routines behave sensibly.
 %
 % Wavefront (wvf) optics suite
-%   wvfComputeConePSF -                       Compute cone psfs given Zernike coeffs, pupil, accom wl, and weighting spectrum
 %   wvfComputeOptimizedConePSF -              Compute focus optimized cone psfs given Zernike coeffs, pupil, accom wl, and weighting spectrum
 %   wvfComputeOptimizedPSF -                  Compute focus optimized (at a specified wl) monochromatic PSFs, ginve Zernike coeffs, etc.
-%
-% Stiles-Crawford Effect (sce)
-%   sceGetParamsParams -                      Get the parameters required for incorporating Stiles-Crawford effect
-%
-% Operate on point spread functions (psf)
-%   psfAverageMultiple -                      Average passed PSFs
-%   psfCenter -                               Put max of PSF at center of image
-%   psfCircularlyAverage -                    Compute circular average of a PSF
-%   psfFindCriterionRadius -                  Find radius that includes passed fraction of PSF mass
-%   psfFindPeak -                             Find the coordinates of the maximum in a matrix.
 %
 % Data
 %   autrusseauStandardObserver.txt -          Zernike coefficient data for the "standard observer" of Autrusseau, Thibos, & Shevell (2011),
