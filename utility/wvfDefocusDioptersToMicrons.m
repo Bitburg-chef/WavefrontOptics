ce8cbd074642d939d8bc7d415ec61469a55fdeba
David Brainard <brainard@psych.upenn.edu>
2012-07-24 14:07:10 -0400

    Verified formula against reference sources
    And added comments as to where it can be found.

--- utility/wvfDefocusDioptersToMicrons.m
+++ utility/wvfDefocusDioptersToMicrons.m
@@ -8,6 +8,32 @@ function microns = wvfDefocusDioptersToMicrons(diopters,pupilSizeMM)
 % Zernike coefficients, that is the size with respect to which the
 % meausurements were made.
 %
+% About this conversion, Heidi says:
+%    The last equation converts between the Zernike defocus coefficient
+%    to a dioptric value (actually the other way), which is not specific
+%    in any way to LCA.
+%
+%    The main consideration in doing this is that the Zernike coefficients
+%    are normalized so that they have unit rms across the pupil- while the dioptric
+%    value is just related to the curvature, so this makes it work out that the zernike defocus
+%    term in microns depends on pupil size for a fixed dioptric power.  Larry Thibos probably
+%    explains this in one of his papers, I had to sit down and draw parabolas with
+%    various pupils to work it out.
+%
+% This formula is also available at the following two sites.
+%   http://www.telescope-optics.net/monochromatic_eye_aberrations.htm
+%   http://arapaho.nsuok.edu/~salmonto/vs2_lectures/Lecture4.pdf
+%
+%   The first source doesn't explicitly give units of pupil diameter,
+%   but it uses mm elsewhere so this is probably
+%   correct.  The same formula is in the second source, except for a sign
+%   difference. We assume the sign to be solely a matter of
+%   convention.
+%
 % 6/5/12  dhb  Wrote it as separate function.
 
-microns = diopters*(pupilSizeMM )^2/(16*sqrt(3));
+% Here is the forward formula from the web site listed above.
+%
+% diopters = (16**sqrt(3))*microns/(pupilSizeMM^2)
+
+microns = diopters*(pupilSizeMM^2)/(16*sqrt(3));
