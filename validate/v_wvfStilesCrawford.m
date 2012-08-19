144538d30771acd3ccf21c08a95a160e2368fb56
David Brainard <brainard@psych.upenn.edu>
2012-08-19 13:26:33 -0400

    Sort out SCE validation script a little more.

--- validate/v_wvfStilesCrawford.m
+++ validate/v_wvfStilesCrawford.m
@@ -1,12 +1,12 @@
 %% v_wvfStilesCrawford
 %
-% Note from DHB.  I'm not sure here what the effect should look like, given
-% that we're using a diffraction limited pupil, so this mostly just
-% demonstrates that the code runs.
+% Note from DHB.  I'm not sure here what the effect should look like, so
+% at present this simply validates that the code runs witout error and
+% that the SCE has an effect on the PSF.
 %
-% Note from HH: This looks about right.
+% 8/19/12  dhb  Updated.
 
-%%
+%% Initialize
 s_initISET
 
 % For plotting limits
@@ -15,38 +15,23 @@ maxMM  = 1;
 waveIdx = 1;
 theWavelength = 550;
 
-%%
+%% Set up wvf structure and sce structure
 wvfParams = wvfCreate;
+wvfParams = wvfSet(wvfParams,'zcoeffs',[0.2 0.75],{'defocus', 'oblique_astigmatism'});
 sceP = sceCreate(theWavelength,'berendschot_data','centered');
 
 %% No Stiles Crawford effect
 wvfParams = wvfSet(wvfParams,'sce params',[]);
 wvfParams = wvfComputePSF(wvfParams);
-
-vcNewGraphWin; 
-hold on
-wvfPlot(wvfParams,'1d psf angle','min',[],maxMIN);
-
-% Make a graph of the PSF within 1 mm of center
-% vcNewGraphWin;
-% wvfPlot(wvfParams,'2dpsf space','mm',maxMM);
+sce1DFig2 = vcNewGraphWin; hold on
+wvfPlot(wvfParams,'1d psf angle','min',[],maxMIN,'no window');
 
 %%  Include the SCE in place
 wvfParams = wvfSet(wvfParams,'sce params',sceP);
 wvfParams = wvfComputePSF(wvfParams);
-% vcNewGraphWin;
-% wvfPlot(wvfParams,'2dpsf space','mm',maxMM);
-
-[f,p] = wvfPlot(wvfParams,'1d psf angle','min',[],maxMIN);
+[f,p] = wvfPlot(wvfParams,'1d psf angle','min',[],maxMIN,'no window');
 set(p,'color','b')
 hold on
 
-wList = wvfGet(wvfParams,'wave');
-strehl = wvfGet(wvfParams,'strehl',wList);
-title(sprintf('Strehl ratio:  %.1f',strehl));
-
-% Figure this out
-% title(sprintf('SCE, returned strehl %0.3f, direct from non-SCE diffrac %0.3f, frac. abs. is %0.2f',...
-%     strehl,max(wvfParams2.psf(:))/max(wvfParams1.psf(:)),wvfParams2.sceFrac));
-
 %% End
+
